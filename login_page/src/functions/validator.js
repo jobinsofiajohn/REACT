f66fc8d4736baec4bb36a3f1b28c89a4a9de9761
jobinsofiajohn <jobinsofiajohn@gmail.com>
2025-03-26 23:47:59 +0530

    created a simple login page that uses javascript for form validation

--- login_page/src/functions/validator.js
+++ login_page/src/functions/validator.js
@@ -1,9 +1,16 @@
-function validator(){
+function validator()
+{
   const name = document.getElementById('name')
   const email = document.getElementById('email')
   const phone = document.getElementById('phone')
   const pwd = document.getElementById('pwd')
   const op = document.getElementById('op')
+
+  name.classList.remove('invalid-input')
+  email.classList.remove('invalid-input')
+  phone.classList.remove('invalid-input')
+  pwd.classList.remove('invalid-input')
+  op.classList.remove('valid')
   
   const emailCheck = !email.value.includes('@') || !email.value.includes('.')
   const phoneCheck = phone.value.length < 10 || !/\d/.test(phone.value)
@@ -12,24 +19,52 @@ function validator(){
 
   const errors=[]
 
-  if(emailCheck) errors.push('Email')
-  if(phoneCheck) errors.push('Phone')
-  if(pwdCheck) errors.push('Password')
-  if(nameCheck) errors.push('Name')
+  if (emailCheck) 
+  {
+    errors.push('Email');
+    email.classList.add('invalid-input');
+  }
+  if (phoneCheck) 
+  {
+    errors.push('Phone');
+    phone.classList.add('invalid-input');
+  }
+  if (pwdCheck) 
+  {
+    errors.push('Password');
+    pwd.classList.add('invalid-input');
+  }
+  if (nameCheck) 
+  {
+    errors.push('Name');
+    name.classList.add('invalid-input');
+  }
 
-  for(let i=0; i<errors.length; i++){
+  for(let i=0; i<errors.length; i++)
+  {
+
     setTimeout(() => op.innerHTML = errors[i] + ' is invalid',
     i*1000)
   }
-  if(emailCheck && phoneCheck && pwdCheck && nameCheck){
+  if(emailCheck && phoneCheck && pwdCheck && nameCheck)
+  {
   setTimeout(() => op.innerHTML = 'Everything is invalid', errors.length*1000)
   }
-  else{
+  else if(errors.length === 0)
+  {
+      op.classList.add('valid');
       setTimeout(() => {
-          op.innerHTML = errors.join(', ') + ' are invalid'
+          op.innerHTML = 'Everything is valid'
       },errors.length*1000)
-    }
   }
+  else
+  {
+      setTimeout(() => {
+          op.innerHTML = errors.join(', ') + ' ' + (errors.length === 1 ? 'is' : 'are') + ' invalid'
+      },errors.length*1000)
+  } 
+  
+}
 
 
 export { validator }
